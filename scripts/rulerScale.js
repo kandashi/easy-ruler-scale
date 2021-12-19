88cf9e3f0d8a4524e1209aed20ebba8772dbe01e
kandashi <jacobmpalmer@live.co.uk>
2021-12-19 19:25:56 +0000

    v9

--- scripts/rulerScale.js
+++ scripts/rulerScale.js
@@ -78,9 +78,9 @@ function newMeasure(destination, {gridSpaces=true}={}) {
             label.position.set(labelPosition.x, labelPosition.y);
             //generate scale modifiers from canvas size (assuming default of 100 pixel) and canvas zoom level
             let scale = game.settings.get("easy-ruler-scale", "scaling")
-            let gs = (canvas.scene.dimensions.size /100) * (scale/100)
+            let gs = (canvas.scene.dimensions.size /100) 
             let zs = 1/canvas.stage.scale.x
-            label.transform.scale.set(gs+zs)
+            label.transform.scale.set((gs+zs) * (scale/100))
         }
 
         // Highlight grid positions
